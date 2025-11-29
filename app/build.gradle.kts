af5adc3bfcbbc224abc1dcaf60cb69356de805b4
Lsapk <lsapk69210@gmail.com>
2025-11-29 20:29:14 +0100

    v6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     // Firebase AI
     implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
     implementation("com.google.firebase:firebase-ai")
-
+    implementation("com.google.firebase:firebase-appcheck-playintegrity")
 
 
     testImplementation("junit:junit:4.13.2")
