5d430aed3473c83fca5d7aa66375534b581503dd
Lsapk <lsapk69210@gmail.com>
2025-12-20 18:54:59 +0100

    v8

--- app/src/main/java/com/deepflowia/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/deepflowia/app/ui/screens/HomeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
